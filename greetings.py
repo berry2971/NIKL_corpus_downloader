7a9ba8339fee52cf538855c3fbd2c672ef670aea
berry2971 <berry2971@hanmail.net>
2020-11-02 00:36:57 +0900

    Delete greetings.py

--- greetings.py
+++
@@ -1,9 +0,0 @@
-def greetings():
-    print()
-    print('Automated downloader for corpus database in NIKL')
-    print('Version: 2.01, Last Updated: 2020. 7. 28')
-    print('업데이트 다운로드: https://www.github.com/berry2971')
-    print()
-    print('코퍼스 데이터베이스 원위치: 국립국어원 언어정보나눔터 > 데이터베이스 자료 > 말뭉치 파일')
-    print('주의사항: 자동으로 실행되는 크롬 인터넷 브라우저를 절대로 임의로 조작하거나 종료하지 마세요.')
-    print()
